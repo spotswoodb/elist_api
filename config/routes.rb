2f4b2bff5299d8f4b51ba0cf5dd9b27a874a70c6
Ben Spotswood <spotswoodb@gmail.com>
2021-09-03 14:54:48 -0500

    change routes so the contacts index page is nested underneath podcasts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :contacts
-  resources :podcasts
+  resources :podcasts do 
+    resources :contacts, only: [:index]
+  end
+
+  resources :contacts, except: [:index]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
